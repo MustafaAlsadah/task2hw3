b7c8bcff204489c6170da49113df4b5773732755
Mustafa Alsadah <mustafaalsadah1@gmail.com>
2024-03-28 05:54:58 +0300

    cmt

---
+++ sub/server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.post("/sub", (req, res) => {
+    const { X, Y } = req.body;
+    const result = X - Y;
+    res.json({ X, Y, Result: result });
+}
+);
+
+app.listen(8080, () => {
+    console.log("Server is running on port 8080");
+});
